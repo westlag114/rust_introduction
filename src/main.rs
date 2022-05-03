572a28a24ffefd57ed42eb9885b7aecfa521875b
westlag114 <fsww2353@gmail.com>
2022-05-03 15:23:40 +0900

    4.1~

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+// fn main() {
+//     let s1 = String::from("hello");
+
+//     let (s2, len) = calculate_length(s1);
+
+//     //'{}'の長さは、{}です
+//     println!("The length of '{}' is {}.", s2, len);
+// }
+
+// fn calculate_length(s: String) -> (String, usize) {
+//     let length = s.len(); // len()メソッドは、Stringの長さを返します
+
+//     (s, length)
+// }
+
+// fn main() {
+//     let s1 = String::from("hello");
+
+//     let len = calculate_length(&s1);
+
+//     println!("The length of '{}' is {}.", s1, len);
+// }
+
+// fn calculate_length(s: &str) -> usize {
+//     s.len()
+// }
+
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("hello");
+
+    change(&mut s);
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
