4d4096acbe71449aa236b712ad3496dc599fa37b
westlag114 <fsww2353@gmail.com>
2022-05-05 19:37:09 +0900

    trait

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,37 @@
-// fn main() {
-//     let s1 = String::from("hello");
-
-//     let (s2, len) = calculate_length(s1);
-
-//     //'{}'の長さは、{}です
-//     println!("The length of '{}' is {}.", s2, len);
-// }
-
-// fn calculate_length(s: String) -> (String, usize) {
-//     let length = s.len(); // len()メソッドは、Stringの長さを返します
-
-//     (s, length)
-// }
-
-// fn main() {
-//     let s1 = String::from("hello");
-
-//     let len = calculate_length(&s1);
-
-//     println!("The length of '{}' is {}.", s1, len);
-// }
-
-// fn calculate_length(s: &str) -> usize {
-//     s.len()
-// }
-
-fn main() {
-    let mut s = String::from("hello");
-
-    change(&mut s);
+trait CalcArea {
+    fn calc_area(&self) -> f64;
 }
 
-fn change(some_string: &mut String) {
-    some_string.push_str(", world");
+struct Rectangle {
+    width: f64,
+    height: f64,
+}
+
+impl CalcArea for Rectangle {
+    fn calc_area(&self) -> f64 {
+        self.width * self.height
+    }
+}
+
+struct RightTriangle {
+    width: f64,
+    height: f64,
+}
+
+impl CalcArea for RightTriangle {
+    fn calc_area(&self) -> f64 {
+        self.width * self.height * 0.5
+    }
+}
+
+fn area<T: CalcArea>(x: &T) -> f64 {
+    x.calc_area()
+}
+
+fn main() {
+    let rect = Rectangle {
+        width: 1.0,
+        height: 2.0,
+    };
+    println!("{}", area(&rect));
 }
