904354096ec93c50af3126afae482bc293043f4c
westlag114 <fsww2353@gmail.com>
2022-05-07 18:50:06 +0900

    stack_heap

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,7 @@
-trait CalcArea {
-    fn calc_area(&self) -> f64;
-}
-
-struct Rectangle {
-    width: f64,
-    height: f64,
-}
-
-impl CalcArea for Rectangle {
-    fn calc_area(&self) -> f64 {
-        self.width * self.height
-    }
-}
-
-struct RightTriangle {
-    width: f64,
-    height: f64,
-}
-
-impl CalcArea for RightTriangle {
-    fn calc_area(&self) -> f64 {
-        self.width * self.height * 0.5
-    }
-}
-
-fn area<T: CalcArea>(x: &T) -> f64 {
-    x.calc_area()
-}
+mod stack_heap;
 
 fn main() {
-    let rect = Rectangle {
-        width: 1.0,
-        height: 2.0,
-    };
-    println!("{}", area(&rect));
+    // println!("memory address is {:p}", &MAX_POINTS)
+
+    stack_heap::run();
 }
